7e9e6af2136842a22ff92b5e31538f17ffc267c2
Billions Joel <joelatugonza@gmail.com>
2022-01-25 14:08:44 +0300

    Install dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
-		filename: 'bundle.js',
+		filename: 'js/bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: './dist',
 	},
 };
