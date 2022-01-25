47f277fef55d3dafba3cac698da1c380e866ddc0
Billions Joel <joelatugonza@gmail.com>
2022-01-25 20:30:52 +0300

    Fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/js/index.js',
-	output: {
-		filename: 'js/bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devServer: {
-		static: './dist',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: './src/index.html',
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-			},
-		],
-	},
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
