bbe52af0534cd20459e68d8b059d7ecc450c0a69
Billions Joel <joelatugonza@gmail.com>
2022-01-25 13:41:12 +0300

    Add new package.json and fix filename spelling

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-		finename: 'bundle.js',
 	},
 	mode: 'development',
 };
